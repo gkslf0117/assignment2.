0ba65ceee3f8a7e2bb03c0ecde320631d0df01a3
gkslf0117 <gkslf0117@gmail.com>
2026-04-20 10:24:47 +0900

    Increase training epochs for model2 from 1 to 3

--- generate_models.py
+++ generate_models.py
@@ -20,7 +20,7 @@ model1.save('models/resnet50_cifar10_v1.h5')
 print("\n=== Model 2 학습 시작 (RMSprop) ===")
 model2 = ResNet50(weights=None, input_shape=(32, 32, 3), classes=10)
 model2.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model2.fit(x_train, y_train, epochs=1, batch_size=128)
+model2.fit(x_train, y_train, epochs=3, batch_size=128)
 model2.save('models/resnet50_cifar10_v2.h5')
 
 print("\n[성공] 모델 2개가 models/ 폴더에 저장됨")
